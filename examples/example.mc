5e0dde4e5779ca3cf1f7c7fd0faa2109d94bc949
jiangmingming <jiangmingming.kyrie@bytedance.com>
2023-04-13 11:05:07 +0800

    feat: code clean

---
+++ examples/example.mc
@@ -0,0 +1,6 @@
+int sub1(int x) { 
+  return x - 10;
+}
+
+sub1(1000000);
+  
--- src/test/parser.test.js
+++ src/test/parser.test.js
@@ -1,4 +1,4 @@
-import { DFAedge } from './parser';
+import { DFAedge } from '../../parser';
 
 test('adds 1 + 2 to equal 3', () => {
   expect(DFAedge(getClosure([0], atoi('i')))).toBe(3);
