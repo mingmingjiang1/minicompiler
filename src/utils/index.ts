4001081dcb2673c922d5045bb1ecd103f182ceeb
jiangmingming <157197768@qq.com>
2023-04-13 11:12:07 +0800

    feat: code clean2

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-const chalk = require('chalk');
+const chalk = require("chalk");
 
 export const findAllIndex = <T>(arr: T[], target: T): number[] => {
   const res: number[] = [];
@@ -12,7 +12,8 @@ export const findAllIndex = <T>(arr: T[], target: T): number[] => {
 
 export function idCnt(start: string, end: string) {
   const res: number[] = [];
-  let startIndex = atoi(start), endIndex = atoi(end);
+  let startIndex = atoi(start),
+    endIndex = atoi(end);
   for (let i = startIndex; i <= endIndex; i++) {
     res.push(i);
   }
@@ -24,13 +25,35 @@ export function atoi(s: string) {
   return s.charCodeAt(0);
 }
 
-export const success = (desc: string,...rest: any[]) => {
-  console.log(
-    chalk.green.bold(`${desc}`) + chalk.white(...rest)
-  );
+export const success = (desc: string, ...rest: any[]) => {
+  console.log(chalk.green.bold(`${desc}`) + chalk.white(...rest));
 };
 
 export const error = (desc: string, ...rest: any[]) => {
-  console.log(
-    chalk.red.bold(`${desc}`) + chalk.white(...rest));
+  console.log(chalk.red.bold(`${desc}`) + chalk.white(...rest));
+};
+
+// 处理转义字符
+export const transformCharacter = (s: string) => {
+  if (s === "\\") {
+    // 说明是转义字符
+    return true;
+  }
+  return false;
+};
+
+export const getWhiteChar = (s: string) => {
+  switch (s) {
+    case "s":
+      // 空白
+      return " ";
+    case "t":
+      // 空白
+      return "\t";
+    case "n":
+      // 空白
+      return "\n";
+    // 多+一个1
+  }
+  return s;
 };
