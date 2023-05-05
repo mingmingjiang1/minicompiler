9e99bf1d0202a68eae256aa370c3ef6028aef196
jiangmingming <157197768@qq.com>
2023-05-05 11:16:10 +0800

    feat: add sh script

---
+++ .sh/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo $1
+
+npm run compile --file $1
+
+# /Users/bytedance/.nvm/versions/node/v14.20.0/bin/npm i && npm run compile
+
+# sleep 5
+# nodeport=$(netstat -lntp | grep "7000" | awk '{print $4}' | awk -F":" '{print $4}')
+# echo $nodeport
+
+# if [ "$nodeport" ]; then
+#   echo -e "\033[32m[ node process start success on dev env !  ]\033[0m"
+# fi
--- src/semantic/check.ts
+++ src/semantic/check.ts
@@ -8,20 +8,20 @@
 5. 变量必须先声明并初始化才能使用
 */
 
-import { str, string } from "@byted-service/thrift-protocol/lib/mappings";
-import exp from "constants";
-import { TOKEN } from "../type";
 import {
   Add_Class,
   Assign_Class,
+  Bool_Class,
   Branch_Class,
   Caller_Class,
   Cond_Class,
+  Div_Class,
   Expression_Class,
   Formal_Class,
   Function_Class,
   Indentifier_Class,
   Int_Contant_Class,
+  Mul_Class,
   Params_Class,
   Return_Class,
   Sub_Class,
@@ -39,172 +39,6 @@ const scopes: Scope = {
   scope: [], // 最顶层是函数作用域
   next: [],
 };
-
-function checkFunction(f: Function_Class) {
-  const { return_type, expressions, formals } = f;
-  // while (formals) {
-  //   const { name, type } = formals;
-  //      scopes.scope.push({
-  //        name,
-  //        type: type === 'int' ? 1 : 0,
-  //      });
-  //    }
-  // }
-  let cur = expressions;
-  // 函数级别的
-  while (cur) {
-    if (cur instanceof Return_Class) {
-      // check return type and actual return
-      const { expr } = cur;
-      const type = switchCase(expr, scopes);
-      if (type !== return_type) {
-        throw new Error("函数返回类型和实际返回类型不一致");
-      }
-    } else if (cur instanceof Assign_Class) {
-      // 声明变量左边的类型和右边相同
-      const { ltype, name, r } = cur;
-      const target = scopes.scope.find((item) => item.name === name);
-      if (target) {
-        throw new Error(`${name} has beeb declared!`);
-      }
-      scopes.scope.push({
-        name,
-        type: ltype,
-      });
-      if (r instanceof Sub_Class) {
-        const { lvalue, rvalue } = r;
-      }
-    } else if (cur instanceof Cond_Class) {
-    } else if (cur instanceof Branch_Class) {
-      let { statementFalse, statementTrue } = cur;
-      const { ifCond } = cur;
-      // ifCond里的变量是要声明过的
-      const scopeTrue: Scope = {
-        scope: [],
-        next: [],
-        parent: scopes,
-      };
-      const scopeFalse: Scope = {
-        scope: [],
-        next: [],
-        // parent,
-      };
-      scopes.next.push(scopeTrue, scopeFalse);
-      while (statementTrue) {
-        switchCase(statementTrue, scopeTrue);
-        statementTrue = statementTrue.next;
-      }
-      while (statementFalse) {
-        switchCase(statementFalse, scopeFalse);
-        statementFalse = statementFalse.next;
-      }
-    } else if (cur instanceof Caller_Class) {
-      // 实参和形参类型以及数量一致
-    }
-    cur = cur.next;
-  }
-}
-
-// 块级作用域级别
-function switchCase(exp: Expression_Class, scope?: Scope): string[] | undefined | string {
-  if (exp instanceof Return_Class) {
-    // check return type and actual return
-    const { expr } = exp;
-  } else if (exp instanceof Params_Class) {
-    const formals: string[] = [];
-    while (exp) {
-      formals.push(switchCase(exp, scope) as string);
-      exp = exp.next;
-    }
-    return formals;
-  } else if (exp instanceof Assign_Class) {
-    // 声明变量左边的类型和右边相同
-    const { ltype, name, r } = exp;
-
-    const target = scope.scope.find((item) => item.name === name);
-    if (target) {
-      throw new Error(`${name} has beeb declared!`);
-    }
-
-    const type = switchCase(r, scope);
-    if (ltype !== type) {
-      throw new Error(`${type} not assigned to ${ltype}`);
-    }
-    scope.scope.push({
-      name,
-      type: ltype,
-    });
-    return ltype;
-  } else if (exp instanceof Cond_Class) {
-  } else if (exp instanceof Caller_Class) {
-    // 实参和形参类型以及数量一致
-    const { id } = exp;
-    
-    const caller = FunctionMap.get(id);
-    if (!caller) {
-      throw new Error(`${id} has not been implemented`);
-    }
-    let { params } = exp;
-    let i = 0;
-    while (params) {
-      const type = switchCase(params, scope);
-      console.log(params, 999, caller.formal_list[i].type)
-      if (type !== caller.formal_list[i].type) {
-        throw new Error(`参数类型不一致: ${type}, ${caller.formal_list[i].type}`);
-      }
-      params = params.next;
-      i++;
-    }
-
-    if (i !== caller.formal_list.length) {
-      throw new Error(`${id} 参数数量不一致`);
-    }
-    return caller.return_type;
-  } else if (exp instanceof Sub_Class || exp instanceof Add_Class) {
-    const { lvalue, rvalue } = exp;
-    const ltype = switchCase(lvalue);
-    const rtype = switchCase(rvalue);
-    console.log('ltype, rtype', ltype, rtype)
-    if (ltype === rtype) {
-      return ltype;
-    } else {
-      throw new Error(`${rtype} and ${ltype} not matched`);
-    }
-  } else if (exp instanceof Indentifier_Class) {
-    console.log(scope, 'scope');
-    const target = scope.scope.find((item) => item.name === (exp  as any).token);
-    if (!target) {
-      throw new Error(`${exp.token} has not been declared!`);
-    }
-    return target.type;
-  } else if (exp instanceof Int_Contant_Class) {
-    return 'int'; // 1暂时表示整型， 0表示bool，2表示void
-  } else if (exp instanceof Branch_Class) {
-    let { statementFalse, statementTrue } = exp;
-    const { ifCond } = exp;
-    // ifCond里的变量是要声明过的
-    const scopeTrue: Scope = {
-      scope: [],
-      next: [],
-      parent: scope,
-    };
-    const scopeFalse: Scope = {
-      scope: [],
-      next: [],
-      parent: scope,
-    };
-    scope.next.push(scopeTrue, scopeFalse);
-    while (statementTrue) {
-      console.log(888887, statementTrue)
-      switchCase(statementTrue, scopeTrue);
-      statementTrue = statementTrue.next;
-    }
-    while (statementFalse) {
-      switchCase(statementFalse, scopeFalse);
-      statementFalse = statementFalse.next;
-    }
-  } 
-}
 
 export function checkMain() {
   const isExisted = FunctionMap.get("main");
@@ -235,14 +69,17 @@ function checkMeta(expr: Expression_Class, scope: Scope, formal_list: { type: st
       console.log(scope, formal_list);
     }
     if (!target) {
+      console.log(expr);
       throw new Error(`作用域链中没找到该变量: ${expr.token}`);
     }
     return target.type;
   } else if (expr instanceof Int_Contant_Class) {
     return 'int';
-  } else if (expr instanceof Add_Class || expr instanceof Sub_Class) {
+  } else if (expr instanceof Add_Class || expr instanceof Sub_Class || expr instanceof Mul_Class || expr instanceof Div_Class) {
     const { lvalue, rvalue } = expr;
     return checkArigthm(expr, lvalue, rvalue, scope, formal_list);
+  } else if (expr instanceof Bool_Class) {
+    return 'bool';
   }
 }
 
@@ -252,7 +89,7 @@ function checkArigthm(expr: Sub_Class | Add_Class, e1: Expression_Class, e2: Exp
   const t1 = checkMeta(e1, scope, formal_list);
   const t2 = checkMeta(e2, scope, formal_list);
   if (t1 !== t2) {
-    throw new Error(`左右两边类型不一致: ${t1}, ${t2}`);
+    throw new Error(`Type '${t1}' is incompatible with type '${t2}', both should are int type`);
   }
   return t1;
 }
@@ -261,7 +98,7 @@ function checkCond(expr: Cond_Class, e1: Expression_Class, e2: Expression_Class,
   const t1 = checkMeta(e1, scope, formal_list);
   const t2 = checkMeta(e2, scope, formal_list);
   if (t1 !== t2) {
-    throw new Error("左右两边类型不一致");
+    throw new Error(`Type '${t1}' is incompatible with type '${t2}', both should are int type`);
   }
   return 'bool';
 }
@@ -277,9 +114,9 @@ function checkCaller(expr: Caller_Class, scope: Scope, formal_list: { type: stri
   let i = 0;
   while (params) {
     const type = switchTest(params, scope, formal_list);
+    console.log(111, params, caller.formal_list, expr)
     if (type !== caller?.formal_list?.[i]?.type) {
-      console.log(111, params, caller.formal_list, expr)
-      throw new Error(`参数类型或者数量不一致: ${type}不能赋值给${caller?.formal_list?.[i]?.type}`);
+      throw new Error(`参数类型或者数量不一致: 实际类型${type}与期待的类型${caller?.formal_list?.[i]?.type}不兼容`);
     }
     params = params.next;
     i++;
@@ -294,7 +131,7 @@ function checkCaller(expr: Caller_Class, scope: Scope, formal_list: { type: stri
 function checkReturn(expr: Return_Class, scope: Scope, formal_list: { type: string; id: string; }[], return_type: string) {
   const type = switchTest(expr.expr, scope, formal_list);
   if (return_type !== type) {
-    throw new Error("错误的返回类型");
+    throw new Error(`Type '${type}' is not assignable to type '${return_type}'.`);
   }
   return type;
 }
@@ -329,13 +166,13 @@ function checkAssign(expr: Assign_Class, scope: Scope, formal_list: { type: stri
   const { ltype, name, r } = expr;
   const target = scopes.scope.find((item) => item.name === name);
   if (target) {
-    throw new Error(`${name} has beeb declared!`);
+    throw new Error(`${name} has been declared!`);
   }
 
   const type = switchTest(r, scope, formal_list);
 
   if (ltype !== type) {
-    throw new Error("赋值语句左右类型不一致");
+    throw new Error(`Type '${type}' is not assignable to type '${ltype}'.`);
   }
 
   scope.scope.push({
@@ -355,7 +192,7 @@ function switchTest(expr: Expression_Class, scope: Scope, formal_list: { type: s
   } else if (expr instanceof Add_Class || expr instanceof Sub_Class) {
     const { lvalue, rvalue } = expr;
     return checkArigthm(expr, lvalue, rvalue, scope, formal_list);
-  } else if (expr instanceof Indentifier_Class || expr instanceof Int_Contant_Class) {
+  } else if (expr instanceof Indentifier_Class || expr instanceof Int_Contant_Class || expr instanceof Bool_Class) {
     return checkMeta(expr, scope, formal_list);
   } else if (expr instanceof Branch_Class) {
     return checkBranch(expr, scope, formal_list);
@@ -385,6 +222,7 @@ function checkFunc(f: Function_Class) {
     expressions = expressions.next;
   }
   if (!hasReturn && return_type !== 'void') {
-    throw new Error("错误的返回类型");
+    throw new Error(`Type 'void' is not assignable to type '${return_type}'.`);
   }
 }
+// Duplicate function implementation.
